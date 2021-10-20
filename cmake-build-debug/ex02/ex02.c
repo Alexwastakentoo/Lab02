bba596b93eb750a42ccdda825213ff2bf2c56c04
Alex <76250062+Alexwastakentoo@users.noreply.github.com>
2021-10-20 19:46:25 +0200

    Exercise 02 Lab 02 fix

--- cmake-build-debug/ex02/ex02.c
+++ cmake-build-debug/ex02/ex02.c
@@ -5,6 +5,7 @@
 
 
 #define MAX_LINE_SIZE 100
+#define SEQ_SIZE 5
 
 int main(){
 
@@ -18,22 +19,17 @@ int main(){
 
         for(int i = 0; i < MAX_LINE_SIZE; i++){
         // horizontal h line checking
-        if(linebuffer[i] == 'h' && hLine < 5){ // check if the element is an 'h' if so increment the counter
+        if(linebuffer[i] == 'h' && hLine < 5 && i <= MAX_LINE_SIZE - SEQ_SIZE){ // check if the element is an 'h' if so increment the counter
             hLine++;
-        } else if(linebuffer[i] != 'h' || linebuffer[i] == '\n'){ // if the element is not an 'h' then increment the line counter iff hLine is big enough
-            if(hLine >= 5){
-                hLineCount++;
-            }
+        } else if(linebuffer[i] != 'h' && hLine >= 5){ // if the element is not an 'h' then increment the line counter iff hLine is big enough
             hLine = 0; // reset hLine
         }
 
         // vertical v line checking
         if(linebuffer[i] == 'v' && vLine[i] < 5){ // check if the element is a 'v' if so we need to mark the index to check it in the next line
             vLine[i]++;
-           } else if(linebuffer[i] != 'v'){
-            if(vLine[i] >= 5){
-                vLineCount++;
-            }
+           } else if(linebuffer[i] != 'v' && vLine[i] >= 5){
+            vLineCount++;
             vLine[i] = 0;
            }
         }
